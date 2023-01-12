f4d7d0a9177991107a199951d27f22716d3b7c14
Dmitrii Skrylev <zivou4@gmal.com>
2023-01-11 18:06:32 -0600

    Add missing model logic to form_states and form_element_question_types

--- db/migrate/20230111033645_create_form_states.rb
+++ db/migrate/20230111033645_create_form_states.rb
@@ -2,6 +2,7 @@ class CreateFormStates < ActiveRecord::Migration[7.0]
   def change
     create_table :form_states do |t|
       t.string :name
+      t.belongs_to :form, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20230111033744_create_form_element_question_types.rb
+++ db/migrate/20230111033744_create_form_element_question_types.rb
@@ -1,6 +1,7 @@
 class CreateFormElementQuestionTypes < ActiveRecord::Migration[7.0]
   def change
     create_table :form_element_question_types do |t|
+      t.string :name
 
       t.timestamps
     end
