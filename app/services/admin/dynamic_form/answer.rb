f3a79bee492b8ae504feae0814d993d630076ecc
Dmitrii Skrylev <zivou4@gmal.com>
2023-01-17 22:58:09 -0600

    Fix Find element logic

--- app/services/admin/dynamic_form/answer.rb
+++ app/services/admin/dynamic_form/answer.rb
@@ -2,22 +2,22 @@ module Admin::DynamicForm
     class Answer < ApplicationService
         attr_reader :form_element_id
                     :repsonse_data
-                    :form_id
 
-        def initialize(form_element_id, form_id, repsonse_data)
+        def initialize(form_element_id, repsonse_data)
             @form_element_id = form_element_id
             @repsonse_data = repsonse_data
-            @form_id = form_id
         end
 
         #
         # TODO: Check response data type
         def call
+            form_element = find_form_element
             FormResponse.create!(
                 form_element_id: form_element_id, 
-                form_id: form_id,
+                form_id: form_element.form_id,
                 repsonse_data: {
-                    response_data_type: repsonse_data
+                    type: response_data_type(form_element),
+                    value: repsonse_data
                 }
             )
         end
@@ -27,8 +27,8 @@ module Admin::DynamicForm
             FormElement.find(form_element_id)
         end
 
-        def response_data_type
-            find_form_element.form_element_question_type.name
+        def response_data_type(form_element)
+            form_element.form_element_question_type.name
         end
     end
 end
