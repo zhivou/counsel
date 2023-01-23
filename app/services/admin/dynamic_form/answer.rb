741a0dc8e9d110635c1a1248d578cc7d49c80946
Dmitrii Skrylev <zivou4@gmal.com>
2023-01-22 23:31:20 -0600

    Working simple answer logic

--- app/services/admin/dynamic_form/answer.rb
+++ app/services/admin/dynamic_form/answer.rb
@@ -1,30 +1,25 @@
 module Admin::DynamicForm
     class Answer < ApplicationService
-        attr_reader :form_element_id
-                    :repsonse_data
-
         def initialize(form_element_id, repsonse_data)
             @form_element_id = form_element_id
             @repsonse_data = repsonse_data
         end
 
-        #
-        # TODO: Check response data type
         def call
             form_element = find_form_element
             FormResponse.create!(
-                form_element_id: form_element_id, 
+                form_element_id: @form_element_id, 
                 form_id: form_element.form.id,
                 repsonse_data: {
                     type: response_data_type(form_element),
-                    value: repsonse_data
+                    value: @repsonse_data
                 }
             )
         end
 
         private
         def find_form_element
-            FormElement.find(form_element_id)
+            FormElement.find(@form_element_id)
         end
 
         def response_data_type(form_element)
